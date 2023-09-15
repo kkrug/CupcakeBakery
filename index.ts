05c3c9f5131e5c35aec3db12e3e3929a3bbfaa7a
Kenny Krug <kennykrug@gmail.com>
2023-09-15 16:26:43 -0400

    index issues

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
-import { KitchenComponent } from './app/kitchen/kitchen.component';
+import { AppModule } from './src/app/app.module';
+import { KitchenComponent } from './src/app/kitchen/kitchen.component';
+import 'zone.js/dist/zone';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .then((moduleRef) => {
